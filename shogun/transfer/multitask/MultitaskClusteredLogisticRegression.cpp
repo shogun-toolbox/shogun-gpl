ccd38e1e74551a5865517d50d94bc455ca021c5d
zhengliangfeng <i_am_shiyi@163.com>
2019-03-07 10:00:51 +0800

    remove include malsar_clustered.h

--- shogun/transfer/multitask/MultitaskClusteredLogisticRegression.cpp
+++ shogun/transfer/multitask/MultitaskClusteredLogisticRegression.cpp
@@ -9,7 +9,6 @@
 
 #include <shogun/transfer/multitask/MultitaskClusteredLogisticRegression.h>
 
-#include <shogun/lib/malsar/malsar_clustered.h>
 #include <shogun/lib/malsar/malsar_options.h>
 #include <shogun/lib/SGVector.h>
 #include <shogun/features/DotFeatures.h>
